43609e75caa5bf12d50e5c760b2125884d412996
Flynn <Flynnoconnell@gmail.com>
2025-02-18 21:42:42 -0500

    moved imports to mbo_utilities

--- lbm_suite2p_python/__init__.py
+++ lbm_suite2p_python/__init__.py
@@ -1,14 +1,6 @@
 from . import _version
-from .utils import (
-    get_metadata,
-    get_files,
-    ops_from_metadata,
-)
 
 __version__ = _version.get_versions()['version']
 
 __all__ = [
-    "get_metadata",
-    "get_files",
-    "ops_from_metadata",
 ]
