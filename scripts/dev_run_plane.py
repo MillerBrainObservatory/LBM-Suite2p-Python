2123272e3843d2d609347586919b756b10d71e0d
Flynn OConnell <flynnoconnell@gmail.com>
2025-05-01 20:30:38 -0400

    dev update

--- scripts/dev_run_plane.py
+++ scripts/dev_run_plane.py
@@ -2,11 +2,32 @@ import lbm_suite2p_python as lsp
 
 save_path = "/home/flynn/lbm_data/demo/output"
 input_path = "/home/flynn/lbm_data/demo/assembled/plane_08.tif"
-lsp.run_plane_any(
+
+ops1 = lsp.run_plane_any(
     input_path=input_path,
     save_path=save_path,
-    keep_raw=True,
-    keep_reg=True,
+    keep_raw=False,
+    keep_reg=False,
+    force_reg=False,
+    force_detect=False,
+)
+
+res1 = lsp.load_planar_results(ops1)
+print(sum(res1["iscell"]))
+
+ops2 = lsp.run_plane_any(
+    input_path=input_path,
+    save_path=save_path,
+    keep_raw=False,
+    keep_reg=False,
     force_reg=False,
     force_detect=True,
 )
+
+res2 = lsp.load_planar_results(ops1)
+
+ops1path = "/home/flynn/lbm_data/demo/ops1.npy"
+ops2path = "/home/flynn/lbm_data/demo/ops2.npy"
+import numpy as np
+np.save(ops1path, ops1)
+np.save(ops2path, ops2)
