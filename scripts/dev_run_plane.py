8c1349ba5d7c767a63e94de3ce044c7ec165079c
Flynn OConnell <flynnoconnell@gmail.com>
2025-05-08 09:39:55 -0400

    dev script

--- scripts/dev_run_plane.py
+++ scripts/dev_run_plane.py
@@ -2,32 +2,30 @@ from pathlib import Path
 import lbm_suite2p_python as lsp
 import mbo_utilities as mbo
 
-raw_path = Path("/home/flynn/lbm_data/demo/raw")
-
+raw_path = Path("/home/flynn/lbm_data/raw")
 bin_out = raw_path.parent.joinpath("bin")
-tiff_out = raw_path.parent.joinpath("tiff")
 
 scan = mbo.read_scan(raw_path)
-mbo.save_as(scan, bin_out, planes=[7], ext="bin")
+# mbo.save_as(scan, bin_out, planes=[7], ext="bin")
+#
+# bin_file = bin_out.joinpath("plane7", "data_raw.bin")
+# ops1 = lsp.run_plane(
+#     input_path=bin_file,
+#     keep_raw=True
+# )
 
-bin_file = bin_out.joinpath("plane7", "data_raw.bin")
-ops1 = lsp.run_plane(
-    input_path=bin_file,
-    keep_raw=True
-)
+tiff_out = raw_path.parent.joinpath("tiff")
+res_out = raw_path.parent.joinpath("results")
+# mbo.save_as(scan, tiff_out, planes=[0, 7, 13], ext="tif")
+files = mbo.get_files(tiff_out, "tif", 2)
+for file in files:
+    ops2 = lsp.run_plane(
+        input_path=file,
+        keep_reg=True,
+        keep_raw=False,
+        force_reg=False,
+        force_detect=True,
+    )
 
-res1 = lsp.load_planar_results(ops1)
-# print(sum(res1["iscell"]))
-
-mbo.save_as(scan, tiff_out, planes=[7], ext="tif")
-file = tiff_out.joinpath("plane_08.tif")
-ops2 = lsp.run_plane(
-    input_path=file,
-    keep_reg=True,
-    keep_raw=False,
-    force_reg=False,
-    force_detect=True,
-)
-
-res2 = lsp.load_planar_results(ops2)
-x = 5
+# res2 = lsp.load_planar_results(ops2)
+# x = 5
