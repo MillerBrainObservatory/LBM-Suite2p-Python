8284b10a7c15326f4f40bd73918cbf2e1e904ff7
Flynn <Flynnoconnell@gmail.com>
2025-05-13 16:09:01 -0400

    dev: update runplane script

--- scripts/dev_run_plane.py
+++ scripts/dev_run_plane.py
@@ -1,17 +1,17 @@
 from pathlib import Path
 import lbm_suite2p_python as lsp
 
-path = Path(r"/home/flynn/lbm_data")
-res_out = path.joinpath("results")
+path = Path(r"D://demo//assembled5")
+res_out = path.parent.joinpath("suite2p_results")
 
 ops = lsp.run_plane(
-    input_path=path.joinpath("assembled", "plane_08.tif"),
+    input_path=path.joinpath("plane_08.tif"),
     save_path=res_out,
     keep_reg=True,
-    keep_raw=False,
+    keep_raw=True,
     force_reg=False,
     force_detect=True,
 )
 
-# res2 = lsp.load_planar_results(ops2)
-# x = 5
+res = lsp.load_planar_results(ops)
+print(res)
