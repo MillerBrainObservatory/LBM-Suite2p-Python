cdc5b11c7161332d6c7fa11dc2d2f39e5025c97d
Flynn OConnell <flynnoconnell@gmail.com>
2025-06-04 15:07:40 -0400

    dev update

--- scripts/dev_run_plane.py
+++ scripts/dev_run_plane.py
@@ -2,26 +2,37 @@ from pathlib import Path
 import lbm_suite2p_python as lsp
 import mbo_utilities as mbo
 
-raw_path = Path(r"/home/flynn/lbm_data/raw")
-# scan = mbo.read_scan(raw_path)
-# scan.roi = None
-# mbo.save_as(
-#     scan,
-#     raw_path / "testing",
-# )
-path = Path(r"/home/flynn/lbm_data/raw/testing/plane7.tif")
-user_ops = {"classifier_path": "/home/flynn/lbm_data/mbo_v3.npy"}
+if __name__ == "__main__":
 
-ops = lsp.run_plane(
-    input_path=path,
-    save_path=path.parent,  # unused for single-z bin files
-    ops=user_ops,
-    keep_reg=True,
-    keep_raw=True,
-    force_reg=False,
-    force_detect=True,
-    debug=True
-)
+    raw_path = Path(r"/home/flynn/lbm_data/raw")
+    scan = mbo.read_scan(
+        raw_path,
+        fix_phase=True,
+        roi=None,
+    )
+    mbo.save_as(
+        scan,
+        raw_path / "full_fov",
+    )
+    scan.roi = 0
+    mbo.save_as(
+        scan,
+        raw_path / "rois",
+    )
 
-res = lsp.load_planar_results(ops)
-print(res)
+    path = Path(r"/home/flynn/lbm_data/raw/testing/plane7.tif")
+    user_ops = {"classifier_path": "/home/flynn/lbm_data/mbo_v3.npy"}
+
+    ops = lsp.run_plane(
+        input_path=path,
+        save_path=path.parent,  # unused for single-z bin files
+        ops=user_ops,
+        keep_reg=True,
+        keep_raw=True,
+        force_reg=False,
+        force_detect=True,
+        debug=True
+    )
+
+    res = lsp.load_planar_results(ops)
+    print(res)
