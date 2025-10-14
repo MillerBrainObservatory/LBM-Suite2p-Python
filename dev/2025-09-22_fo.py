dbfc22d213a2c273b3533d508e012a3f02bbd7cf
Flynn <75346097+FlynnOConnell@users.noreply.github.com>
2025-10-14 11:58:18 -0400

    bump v2.0.1 (#27)
    * remove mkl dep
    
    * force mbo_utils 1.0.0
    
    * demos-tests
    
    * add dev option
    
    * pin mbo_utilities, lock
    
    * use dev mbo_utilities
    
    * import catch, docs with suite2p_mbo
    
    * add wgpu error to readme
    
    * update lock, pin mbo to 1.0.0
    
    * dev: s3d
    
    * temp dev
    
    * deps: remove pyqt
    
    * update docs and reqs
    
    * docs: remove scale
    
    * docs: run on push to dev, with docs commit msg
    
    * fix deps
    
    * docs: allow prerelease flag
    
    * docs: only run test if ci
    
    * docs: add extraction imgs
    
    * update install
    
    * docs: fix reg link/images in gallery
    
    * bugfix: update metadata shape for single-roi case
    
    * bugfix: skip detection if already ran or not force_reg = True
    
    * feat: load_planar_results now accepts a path to a planar results folder
    
    * give type to data, last check for ops['roidetect'] before shipping
    
    * bugfix: check that ops path exists before raising error
    
    * fix isinstance check on list
    
    * use mbo_utilities logger, refact normalize_folder for reuse
    
    * dev updates
    
    * add save_ops, load_ops returns {}
    
    * get z_plane index from metadata
    
    * dev updates
    
    * add check for few traces
    
    * use mbo log, fix save-path and normalizing folders
    
    * redundant import
    
    * update lock
    
    * dev update
    
    * ruff, black
    
    * ruff
    
    * rename to dff_rolling_percentile
    
    * add cellpose to reqs
    
    * fix rolling dff window caluclation
    
    * make plot_traces use signal-units dffp
    
    * add excludes
    
    * dev ops variants
    
    * dev run plane
    
    * dev: dff stuff
    
    * cleanup
    
    * remove suite2p check
    
    * bugfix: save ops after forcing registration
    
    * add should_write_ops helper
    
    * make sure plane number is in metadata
    
    * update dev scripts
    
    * pin cellpose 4, numpy 2, imgui-bundle v1.6
    
    * remove unused imports
    
    * bugfix: segmentation plots not taken from same sample
    
    * cache rastermap model
    
    * update grid-search
    
    * docs: remove author
    
    * derive tag, update run volume for v2
    
    * remove s3d code -> move to pipelines
    
    * clip num displayed traces to 30
    
    * better data desc for reg docs
    
    * update envs
    
    * update quickstart nb
    
    * fix plot_traces, add median dff option
    
    * bugfix dff scale-bar for percentiles
    
    * remove old nwb code from quickstart
    
    * ruff format, py version to 3.12.7+
    
    * update workflow to 3.12.7
    
    * update version, lockfile 1.0.5
    
    * bugfix: memory leak in run_volume
    
    * add benchmarks, imports
    
    * del ops file after plane run
    
    * show mem/cpu after each zplane run
    
    * fix import clashes
    
    * remove redundant packages
    
    * bugfixes, prep update for z-plane alignment
    
    * coerce str to Path
    
    * fix sources to use pypi index
    
    * remove ops dict check
    
    * bugfix mbo_utilities import version
    
    * data validation, refactor modules
    
    * if roidetect, check for existing stat.npy
    
    * better pytorch deps, merging improvements
    
    * error if processing raw file
    
    * fix ops-override, auto-merging by default with roi* dirs
    
    * bugfix: if data.bin or reg_tif, skip .bin creation
    
    * ruff, group planes, extract demo
    
    * remove print statement
    
    * fix roidetect not being recognized
    
    * post-processing, fix defaults, refactor funcs
    
    * Merge tiling (#26)
    
    * fix merging of 3+ roi, suite2p+cellpose
    
    * remove erroneous prints
    
    * ci: don't install all extras
    
    * add filter for cellpose anomolies
    
    * fix pre-made ops overriding user supplied ops
    
    * bugfix traces use proper iscell_mask
    
    * dev update
    
    * update acknoledgements
    
    * push version
    
    * update lockfile, pin mbo-util >=2.0.0
    
    * update quickstart
    
    * pin mbo_utilities 2.0.0
    
    * upgrade lock, pin mbo_utilities 2.0.2
    
    * bump 2.0.1
    
    * dev: fix workflow dev deps
    
    * update lock for 2.0.1
    
    * remove dev nbs
    
    * remove old nbs

--- dev/2025-09-22_fo.py
+++
@@ -1,7 +0,0 @@
-from pathlib import Path
-import mbo_utilities as mbo
-import lbm_suite2p_python as lsp
-
-ops = {"dff_percentile": 50}
-input_files = mbo.get_files(r"D:\W2_DATA\wsynder", 'tif')
-lsp.run_volume(input_files, r"D:\W2_DATA\wsynder\results", ops=ops)
