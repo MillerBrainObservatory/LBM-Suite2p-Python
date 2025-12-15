a6add9eeefec80f0160083bad0e0f1d429f368c4
Flynn <Flynnoconnell@gmail.com>
2025-12-15 13:42:13 -0500

    remove old doc scripts

--- docs/compare.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-REM Compare documentation before/after uncommitted changes
-cd /d "%~dp0"
-uv run python compare_docs.py %*
--- docs/serve.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Serve documentation on port 8888
-cd "$(dirname "$0")"
-uv run python serve_docs.py 8888
